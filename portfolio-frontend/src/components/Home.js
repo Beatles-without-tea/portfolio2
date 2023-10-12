a0b9fba9b69f571be63f346cef6db4b2afaa31dc
beatles_without_tea <max.beales@gmail.com>
2023-10-12 10:02:01 +0200

    started project page

--- portfolio-frontend/src/components/Home.js
+++ portfolio-frontend/src/components/Home.js
@@ -1,14 +1,22 @@
 import React, { useEffect, useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faNewspaper} from '@fortawesome/free-brands-svg-icons';
 import '../Home.css';
 
 function Home() {
     const textRef = useRef(null);
+    const textRef2 = useRef(null);
+
+
     const imageRef = useRef(null);
 
     useEffect(() => {
         // Add the sliding effect after the component mounts
         if (textRef.current) {
             textRef.current.classList.add('slide-in-text');
+        }
+        if (textRef2.current) {
+            textRef2.current.classList.add('slide-in-text');
         }
         if (imageRef.current) {
             imageRef.current.classList.add('slide-in-image');
@@ -17,12 +25,20 @@ function Home() {
 
     return (
         <div className="homepage">
-            <h1 ref={textRef}>Max Beales <br/> 
-                MSc <span style={{color:'#66FF00'}}>Computer Science </span>  
-                graduate <br/> 
-                <span style={{color:'#66FF00'}}>Data</span> Enthusiast
-            </h1>
+            <div style = {{display:'block'}}>
+                <h1 ref={textRef}>Max Beales <br/> 
+                    MSc <span style={{color:'#66FF00'}}>Computer Science </span>  
+                    graduate <br/> 
 
+                    <span style={{color:'#66FF00'}}>Data</span> Enthusiast
+                </h1>
+                <br></br>
+                <p ref={textRef2}>  1.5 years as a intern Data Scientist
+                <br/> Experienced with Python, SQL, and a range of other tools
+                <br/> Bilingual in French and English
+                <br/> Currently looking for new opportunities !</p>
+           
+            </div>
             <img src="/images/home_image.png" alt="Descriptive Alt Text" ref={imageRef} />
         </div>
     );
--- portfolio-frontend/src/components/Projects.js
+++ portfolio-frontend/src/components/Projects.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import '../Projects.css';
+
 
 function Projects() {
     return (
-        <div>
-            <h1>asdgdfsdfsdfsdg</h1>
-            <p>Tsdfssdfd.</p>
+        <div className='projectpage' >
+
+{/* this is one project         */}
+        <div className='project'>
+            <div className='projectText'>
+                <h1 style={{color:'#66FF00'}} >test</h1>
+                <h2>sduub</h2>
+            </div>
+            <div className='projectImage'>
+                <img src='images/monte_carlo_project.png'></img>
+            </div>
+        </div>
+{/* end of project */}
+
+
+
         </div>
     );
 }
